0a7504459a8cc0cee50bd7921ccc93852ede5c59
Your Name <you@example.com>
2024-06-17 14:52:37 +0200

    feat/terminal.js

---
+++ sources/js/terminal.js
@@ -0,0 +1,14 @@
+// # Initialiser un dépôt Git local
+// git init
+
+// # Ajouter tous les fichiers au dépôt local
+// git add .
+
+// # Effectuer un commit avec un message
+// git commit -m "Initial commit"
+
+// # Ajouter le dépôt GitHub comme origine distante
+// git remote add origin https://github.com/USERNAME/REPOSITORY.git
+
+// # Pousser les fichiers vers GitHub
+// git push -u origin master  # Ou 'main' si votre branche par défaut est 'main'
